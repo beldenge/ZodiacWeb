d7decb5dfcc2286e670c3cc7fd6400727e9b3acb
beldenge <beldenge@gmail.com>
2012-10-26 00:06:27 -0400

    initial commit

---
+++ grails-app/conf/DataSource.groovy
@@ -0,0 +1,43 @@
+dataSource {
+    pooled = true
+    driverClassName = "org.postgresql.Driver"
+    username = "postgres"
+    password = "postgres"
+}
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = false
+    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+}
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            dbCreate = "validate" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:postgresql://localhost:5432/Zodiac"
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "validate"
+            url = "jdbc:postgresql://localhost:5432/Zodiac"
+        }
+    }
+    production {
+        dataSource {
+            dbCreate = "validate"
+            url = "jdbc:postgresql://localhost:5432/Zodiac"
+            pooled = true
+            properties {
+               maxActive = -1
+               minEvictableIdleTimeMillis=1800000
+               timeBetweenEvictionRunsMillis=1800000
+               numTestsPerEvictionRun=3
+               testOnBorrow=true
+               testWhileIdle=true
+               testOnReturn=true
+               validationQuery="SELECT 1"
+            }
+        }
+    }
+}
---
+++ grails-app/controllers/zodiacweb/SolutionSetController.groovy
@@ -0,0 +1,8 @@
+package zodiacweb
+
+import com.ciphertool.zodiacengine.entities.SolutionSet
+
+class SolutionSetController {
+
+    static scaffold = SolutionSet
+}
